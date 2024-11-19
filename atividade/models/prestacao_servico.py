9daa49bde038514711453e99f31f81d56144b430
Eliel Santos <161032378+Xheliel@users.noreply.github.com>
2024-11-19 13:22:48 +0000

    Prest

--- atividade/models/prestacao_servico.py
+++ atividade/models/prestacao_servico.py
@@ -1,13 +1,15 @@
 from models.abstracts.juridica import Juridico
 
 class Fornecedor(Juridico):
-    def __init__(self, produto: str, nome, telefone, email, endereco, cnpj, inscricao_estadual):
+    def __init__(self, contrato_inicio: str, contrato_fim: str, nome, telefone, email, endereco, cnpj, inscricao_estadual):
         super().__init__(nome, telefone, email, endereco, cnpj, inscricao_estadual)
-        self.produto = produto
+        self.contrato_inicio = contrato_inicio
+        self.contrato_fim = contrato_fim
 
     def __str__(self):
         return (
             f"{super().__str__()}"
-            f"\nProduto: {self.produto}"
+            f"\nInício do contrato: {self.contrato_inicio}"
+            f"\nFim do contrato: {self.contrato_fim}"
         )
     
